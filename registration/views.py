92adb7ae5817ec4667e6ecbe7ba6f61fcee44157
danielmosomi <danielmosomi51@gmail.com>
2024-04-08 11:24:42 +0300

    django

---
+++ registration/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def registration(request):
+    return HttpResponse("Welcome to registration")
+
+def login(request):
+    template=loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+# Create your views here.
