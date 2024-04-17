a2b2502f6cc40d9323d0e41af50fcd76a6bee262
danielmosomi <danielmosomi51@gmail.com>
2024-04-17 12:45:08 +0300

    fourth commit

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'hospital_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'hospital.sqlite3',
     }
 }
 
